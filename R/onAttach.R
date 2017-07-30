c8da237428909ec23e8ccb5f7976bd2dbf6405fc
Richard White <a@rwhite.no>
2017-07-30 12:05:38 +0200

    Versioning

---
+++ R/onAttach.R
@@ -0,0 +1,5 @@
+.onAttach <- function(libname, pkgname) {
+  packageStartupMessage('PACKAGE: normomo')
+  packageStartupMessage('Version')
+  packageStartupMessage('Developed by Richard White, Norwegian Institute of Public Health')
+}
