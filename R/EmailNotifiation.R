a90bf48104c5735a2c00a515f955aa7ffe9cd5a7
Richard White <r@rwhite.no>
2018-04-12 15:53:25 +0300

    emails will now maybe include results

--- R/EmailNotifiation.R
+++ R/EmailNotifiation.R
@@ -19,15 +19,79 @@
 # }
 
 #' blah
+#' @param folderResultsYearWeek a
+#' @param isTest a
 #' @importFrom fhi DashboardEmail
-#' @export EmailNotificationOfNewResults
-EmailNotificationOfNewResults <- function(){
+#' @export EmailInternal
+EmailInternal <- function(folderResultsYearWeek, isTest = TRUE){
   emailText <- "New NorMOMO results available to download from:
 <br><br>
-<a href='http://smhb.fhi.no/s/normomo/'>http://smhb.fhi.no/s/normomo/</a>
+<a href='file:///F:/Prosjekter/Dashboards/results/normomo/'>F:/Prosjekter/Dashboards/results/normomo/</a>
 "
 
-  fhi::DashboardEmail("normomo_results",
-                  "New NorMOMO results available",
-                  emailText)
+  if (isTest) {
+    fhi::DashboardEmail(
+      "normomo_test",
+      emailSubject = "TESTING EmailInternal",
+      emailText
+    )
+  } else {
+    fhi::DashboardEmail(
+      "normomo_results",
+      "New NorMOMO results available",
+      emailText
+    )
+  }
+}
+
+
+#' blah
+#' @param folderResultsYearWeek a
+#' @param isTest a
+#' @importFrom fhi DashboardEmail
+#' @export EmailSSI
+EmailSSI <- function(folderResultsYearWeek, isTest = TRUE){
+
+  currentYearWeek <- stringr::str_extract(folderResultsYearWeek,"[0-9]*-[0-9]*")
+
+  files <- list.files(file.path(folderResultsYearWeek,"MOMO"))
+
+  folderNorway1 <- files[stringr::str_detect(files,"Norway")]
+  files <- list.files(file.path(folderResultsYearWeek,"MOMO",folderNorway1))
+
+  folderNorway2 <- files[stringr::str_detect(files,"COMPLETE")]
+  files <- list.files(file.path(folderResultsYearWeek,"MOMO",folderNorway1,folderNorway2))
+
+  attachFiles <- file.path(folderResultsYearWeek,"MOMO",folderNorway1,folderNorway2,files)
+
+  emailText <- "
+<html><body>
+Dear EuroMOMO hub,<br><br>
+Please find attached the current week's results.<br><br>
+Sincerely,<br>
+Norway<br><br><br>
+------------------------
+<br>
+DO NOT REPLY TO THIS EMAIL! This email address is not checked by anyone!
+<br>
+To add or remove people to/from this notification list, send their details to richardaubrey.white@fhi.no
+</body></html>"
+
+  if (isTest) {
+    DashboardEmail(
+      emailBCC="normomo_test",
+      emailSubject = sprintf("TESTING EmailSSI [euromomo input] [Norway] [%s]",stringr::str_replace(currentYearWeek,"-"," ")),
+      emailText,
+      emailAttachFiles=attachFiles,
+      emailFooter=FALSE,
+      BCC=FALSE
+    )
+  } else {
+    fhi::DashboardEmail(
+      "normomo_ssi",
+      emailSubject = sprintf("[euromomo input] [Norway] [%s]",stringr::str_replace(currentYearWeek,"-"," ")),
+      emailText,
+      emailAttachFiles=attachFiles
+    )
+  }
 }
